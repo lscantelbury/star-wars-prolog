e75db2669fc5e7f98e4a5215c5fd171c41472afe
Diógeles Da Silva Tamaturgo <83620550+tamaturgo@users.noreply.github.com>
2022-02-27 18:34:13 -0400

    sort personagens

--- personagens.pl
+++ personagens.pl
@@ -1,83 +1,83 @@
-anakin('Anakin Skywalker').
-darth_vader('Darth Vader').
-obi_wan('Obi-Wan Kenobi').
-luke('Luke Skywalker').
-yoda('Mestre Yoda').
-palpatine('Palpatine').
-han_solo('Han Solo').
-ben_solo('Ben Solo').
-chewbacca('Chewbacca').
-dookan('Conde Dookan').
-darth_tyranus('Darth Tyranus').
-lando('Lando Calrissian').
-windu('Mace Windu').
-qui_gon('Qui-Gon Jinn').
-leia('Leia Organa').
-padme('Padmé Amidala').
-rey('Rey Skywalker').
-ben('Ben Solo').
-kylo('Kylo Ren').
-mesmaPessoa(ben, kylo).
-mesmaPessoa(anakin, darth_vader).
-ahsoka('Ahsoka Tano').
-darth_sidious('Darth Sidious').
-r2d2('R2-D2').
-c3po('C-3PO').
-finn('Finn').
-tarkin('Grand Moff Tarkin').
-darth_maul('Darth Maul').
-poe('Poe Dameron').
-jabba('Jabba the Hutt').
-baby_yoda('Baby Yoda').
-mesmaPessoa(gogru, baby_yoda).
-phasma('Capitã Phasma').
+aayla('Aayla Secura').
 ackbar('Almirante Ackbar').
+ahsoka('Ahsoka Tano').
+anakin('Anakin Skywalker').
+baby_yoda('Baby Yoda').
+bane('Cad Bane').
 bb8('BB-8').
-jar_jar('Jar Jar Binks').
-grievous('General Grievous').
-hux('General Hux').
-finis('Finis Valorum').
-piett('Almirante Piett').
-nute('Nute Gunray').
-shmi('Shmi Skywalker').
-wicket('Wicket W. Warrick').
-watto('Watto').
-jobal('Jobal Naberrie').
-ruweee('Ruweee Naberrie').
+ben('Ben Solo').
+ben_solo('Ben Solo').
+bo_katan('Bo-Katan Kryze').
+boba('Boba Fett').
+c3po('C-3PO').
+cara_dune('Cara Dune').
+chewbacca('Chewbacca').
 cliegg('Cliegg Lars').
+darth_andeddu('Darth Andeddu').
+darth_badon('Darth Badon').
 darth_bane('Darth Bane').
+darth_caedus('Dart Caedus').
+darth_cognus('Darth Cognus').
 darth_krayt('Darth Krayt').
 darth_malak('Darth Malak').
-darth_caedus('Dart Caedus').
-darth_andeddu('Darth Andeddu').
-darth_cognus('Darth Cognus').
-darth_badon('Darth Badon').
+darth_maul('Darth Maul').
 darth_nihilus('Darth Nihilus').
-darth_ruin('Darth Ruin').
-mara_jade('Mara Jade Skywalker').
-kit_fisto('Kit Fisto').
-luminara('Luminara Unduli').
-ki_adi('Ki-Adi-Mundi').
-shaak_ti('Shaak Ti').
-plo_koon('Plo Koon').
-koth('Eeth Koth')
-paratus('Kazdan Paratus').
-poof('Yarael Poof').
-seasee('Saesee Tiin').
-bane('Cad Bane').
-din('Din djarin').
-gogru('Gogru').
-cara_dune('Cara Dune').
-paz_vizla('Paz Vizla').
-fennec('Fennec Shand').
-fortuna('Bib Fortuna').
-boba('Boba Fett').
-bo_katan('Bo-Katan Kryze').
-jango('Jango Fett').
-kanata('Maz Kanata').
-krrsantan('Krrsantan').
 darth_revan('Darth Revan').
+darth_ruin('Darth Ruin').
+darth_sidious('Darth Sidious').
+darth_tyranus('Darth Tyranus').
+darth_vader('Darth Vader').
+din('Din djarin').
+dookan('Conde Dookan').
+fennec('Fennec Shand').
+finis('Finis Valorum').
+finn('Finn').
+fortuna('Bib Fortuna').
+gogru('Gogru').
+grievous('General Grievous').
+han_solo('Han Solo').
+hux('General Hux').
+jabba('Jabba the Hutt').
+jango('Jango Fett').
+jar_jar('Jar Jar Binks').
+jobal('Jobal Naberrie').
 jyn('Jyn Erso').
+kanata('Maz Kanata').
+ki_adi('Ki-Adi-Mundi').
+kit_fisto('Kit Fisto').
+koth('Eeth Koth')
+krrsantan('Krrsantan').
+kylo('Kylo Ren').
+lando('Lando Calrissian').
+leia('Leia Organa').
+luke('Luke Skywalker').
+luminara('Luminara Unduli').
+mara_jade('Mara Jade Skywalker').
+mesmaPessoa(anakin, darth_vader).
+mesmaPessoa(ben, kylo).
+mesmaPessoa(gogru, baby_yoda).
+nute('Nute Gunray').
+obi_wan('Obi-Wan Kenobi').
+padme('PadmÃ© Amidala').
+palpatine('Palpatine').
+paratus('Kazdan Paratus').
+paz_vizla('Paz Vizla').
+phasma('CapitÃ£ Phasma').
+piett('Almirante Piett').
+plo_koon('Plo Koon').
+poe('Poe Dameron').
+poof('Yarael Poof').
+qui_gon('Qui-Gon Jinn').
+r2d2('R2-D2').
+rey('Rey Skywalker').
+ruweee('Ruweee Naberrie').
 sabine('Sabine Wren').
-aayla('Aayla Secura').
 satine('Satine Kryze').
+seasee('Saesee Tiin').
+shaak_ti('Shaak Ti').
+shmi('Shmi Skywalker').
+tarkin('Grand Moff Tarkin').
+watto('Watto').
+wicket('Wicket W. Warrick').
+windu('Mace Windu').
+yoda('Mestre Yoda').
