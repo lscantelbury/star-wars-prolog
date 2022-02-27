4caf5cc2cf1058e375e0986938a1b64cc6155af9
Diógeles Da Silva Tamaturgo <83620550+tamaturgo@users.noreply.github.com>
2022-02-27 18:38:14 -0400

    sort Parentes

--- parentes.pl
+++ parentes.pl
@@ -1,33 +1,30 @@
-:- include('personagens.pl').
-:- include('mulheres.pl').
 :- include('homens.pl').
-
-mae(shmi, anakin).
-mae(jobal, padme).
-mae(padme, leia).
-mae(padme, luke).
-mae(leia, ben_solo).
-
-pai(cliegg, anakin).
-pai(ruweee, padme).
-pai(anakin, luke).
-pai(anakin, leia).
-pai(han_solo, ben_solo).
-
-casal(cliegg, shmi).
-casal(ruweee, jobal).
-casal(anakin, padme).
-casal(han_solo, leia).
-
-irmaos(X, Y) :- pai(Z, X), pai(Z, Y), mae(M, X), mae(M, Y), X \= Y.
-
-irma(X, Y) :- irmaos(X, Y), mulher(X).
-
-irmao(X, Y) :- irmaos(X, Y), homem(X).
-
-tio(X, Y) :- (pai(Z, Y); mae(Z, Y)), irmao(Z, X).
-tia(X, Y) :- (pai(Z, Y); mae(Z, Y)), irma(Z, X).
+:- include('mulheres.pl').
+:- include('personagens.pl').
 
 avo(X, Y) :- (pai(P, Y); mae(P, Y)), pai(X, P).
-
 avoh(X, Y) :- (mae(M, Y); pai(M, Y)), mae(X, M).
+
+casal(anakin, padme).
+casal(cliegg, shmi).
+casal(han_solo, leia).
+casal(ruweee, jobal).
+
+irma(X, Y) :- irmaos(X, Y), mulher(X).
+irmao(X, Y) :- irmaos(X, Y), homem(X).
+irmaos(X, Y) :- pai(Z, X), pai(Z, Y), mae(M, X), mae(M, Y), X \= Y.
+
+mae(jobal, padme).
+mae(leia, ben_solo).
+mae(padme, leia).
+mae(padme, luke).
+mae(shmi, anakin).
+
+pai(anakin, leia).
+pai(anakin, luke).
+pai(cliegg, anakin).
+pai(han_solo, ben_solo).
+pai(ruweee, padme).
+
+tia(X, Y) :- (pai(Z, Y); mae(Z, Y)), irma(Z, X).
+tio(X, Y) :- (pai(Z, Y); mae(Z, Y)), irmao(Z, X).
